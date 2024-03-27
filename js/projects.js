ce724ad3cb037c5b722edbfb72ac4145bf7bf584
d1r0l <xd1r0lx@gmail.com>
2024-03-27 14:28:24 +0200

    mixed projects boilerplate

--- js/projects.js
+++ js/projects.js
@@ -18,13 +18,11 @@ const projects = [
     altScreenshot: 'screenshot of Bloglist webpage'
   },
   {
-    title: 'Bloglist',
-    description:
-      'First advanced project with React & Node.js backend server connected to Mongo.DB',
-    link: 'https://bloglist.d1r0l.xyz/',
-    gitLink: 'https://github.com/d1r0l/bloglist',
-    screenshot: './assets/img/screenshots/Screenshot_BlogList.png',
-    altScreenshot: 'screenshot of Bloglist webpage'
+    title: 'Blank Page',
+    description: 'Blank description',
+    link: '',
+    gitLink: '',
+    altScreenshot: 'screenshot of blank webpage'
   },
   {
     title: 'Bloglist',
@@ -36,22 +34,12 @@ const projects = [
     altScreenshot: 'screenshot of Bloglist webpage'
   },
   {
-    title: 'Bloglist',
+    title: 'Lorem ipsum dolor sit amet',
     description:
-      'First advanced project with React & Node.js backend server connected to Mongo.DB',
-    link: 'https://bloglist.d1r0l.xyz/',
-    gitLink: 'https://github.com/d1r0l/bloglist',
-    screenshot: './assets/img/screenshots/Screenshot_BlogList.png',
-    altScreenshot: 'screenshot of Bloglist webpage'
-  },
-  {
-    title: 'Bloglist',
-    description:
-      'First advanced project with React & Node.js backend server connected to Mongo.DB',
-    link: 'https://bloglist.d1r0l.xyz/',
-    gitLink: 'https://github.com/d1r0l/bloglist',
-    screenshot: './assets/img/screenshots/Screenshot_BlogList.png',
-    altScreenshot: 'screenshot of Bloglist webpage'
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non augue vitae velit semper convallis. Integer bibendum pulvinar tellus non vestibulum. Vestibulum convallis consequat nisi ac rhoncus.',
+    link: '',
+    gitLink: '',
+    altScreenshot: 'screenshot of Lorem webpage'
   }
 ]
 
